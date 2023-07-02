543df234decd09b6d585fe734eb0814f09c14307
wangdi-linux <wangdi7670@163.com>
2023-07-02 17:05:50 +0800

    修改 proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,4 +104,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int interval;                // sigalarm-interval
+  void (*handler)();           // sigalarm-handler
+  int ticksFromLast;           // how many ticks have passed since the last call
 };
