9859238985d73d81d7dbd207082046eeace7be99
WangDi <wangdi7670@163.com>
2023-09-17 19:24:32 +0800

    add comment for buf.h/buf.refcnt

--- kernel/buf.h
+++ kernel/buf.h
@@ -4,7 +4,7 @@ struct buf {
   uint dev;
   uint blockno;
   struct sleeplock lock;
-  uint refcnt;
+  uint refcnt;  // refcnt > 0 indicates that there is at least one process using it(get the sleep-lock) or waiting for it(not get the sleep-lock)
   struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
