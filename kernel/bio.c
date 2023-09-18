1617a0aace6353d2ed896330696e3e3d93917b37
WangDi <wangdi7670@163.com>
2023-09-19 00:17:59 +0800

    一个很离谱的bio.c，一开始的错误是 b->refcnt < 0，后来又冒出了个新错误"panic: ilock: no type" 决定放弃这一版了，重新写 这也告诉我，写代码时要时不时的commit一下，要不然都找不到自己改哪了

--- kernel/bio.c
+++ kernel/bio.c
@@ -24,6 +24,17 @@
 #include "buf.h"
 
 struct {
+  int bget_889;
+  int brealese_889;
+  int bpin_889;
+  int bunpin_889;
+} log889;
+
+
+// #define FLAG
+#ifdef FLAG
+
+struct {
   struct spinlock lock;
   struct buf buf[NBUF];
 
@@ -50,6 +61,11 @@ binit(void)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
+
+  log889.bget_889 = 0;
+  log889.bpin_889 = 0;
+  log889.brealese_889 = 0;
+  log889.bunpin_889 = 0;
 }
 
 // Look through buffer cache for block on device dev.
@@ -61,6 +77,9 @@ bget(uint dev, uint blockno)
   struct buf *b;
 
   acquire(&bcache.lock);
+  if (blockno == 889) {
+    log889.bget_889++;
+  }
 
   // Is the block already cached?
   for(b = bcache.head.next; b != &bcache.head; b = b->next){
@@ -123,6 +142,10 @@ brelse(struct buf *b)
 
   acquire(&bcache.lock);
   b->refcnt--;
+  if (b->blockno == 889) {
+    log889.brealese_889--;
+  }
+
   if (b->refcnt == 0) {
     // no one is waiting for it.
     b->next->prev = b->prev;
@@ -132,13 +155,17 @@ brelse(struct buf *b)
     bcache.head.next->prev = b;
     bcache.head.next = b;
   }
-  
+
   release(&bcache.lock);
 }
 
 void
 bpin(struct buf *b) {
   acquire(&bcache.lock);
+  
+  if (b->blockno == 889) {
+    log889.bpin_889++;
+  }
   b->refcnt++;
   release(&bcache.lock);
 }
@@ -146,8 +173,257 @@ bpin(struct buf *b) {
 void
 bunpin(struct buf *b) {
   acquire(&bcache.lock);
+  if (b->blockno == 889) {
+    log889.bunpin_889--;
+  }
   b->refcnt--;
   release(&bcache.lock);
 }
 
+#else
 
+#define TABLE_SIZE 15
+#define BUCKET_SIZE 2
+
+struct bucket {
+  struct spinlock lock;
+  struct buf buf[BUCKET_SIZE];
+  struct buf head;
+};
+
+
+struct {
+  struct spinlock lock;
+  struct bucket buf_table[TABLE_SIZE];
+} bcache;
+
+
+void test_init();
+int bucket_length(struct bucket *bkt);
+
+
+
+void bucket_init(struct bucket *bkt)  // 2. 
+{
+  struct buf *b;
+
+  initlock(&bkt->lock, "bcache_bkt");
+
+  // Create linked list of buffers
+  bkt->head.prev = &bkt->head;
+  bkt->head.next = &bkt->head;
+
+  for(b = bkt->buf; b < bkt->buf + BUCKET_SIZE; b++){
+    b->next = bkt->head.next;
+    b->prev = &bkt->head;
+    initsleeplock(&b->lock, "buffer");
+    bkt->head.next->prev = b;
+    bkt->head.next = b;
+
+/*     b->refcnt = 0;
+    b->valid = 0;
+    b->blockno = 0;
+    b->dev = 0; */
+  }
+
+}
+
+
+void binit()
+{
+  initlock(&bcache.lock, "bcache");
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    bucket_init(&bcache.buf_table[i]);
+  }
+
+  test_init();
+}
+
+
+void test_init()
+{
+  int count = 0;
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    struct bucket *bkt = &bcache.buf_table[i];
+    count += bucket_length(bkt);
+  }
+
+  if (count != TABLE_SIZE * BUCKET_SIZE) {
+    panic("wrong init");
+  }
+
+  printf("init passed OK==\n");
+}
+
+
+int bucket_length(struct bucket *bkt)
+{
+  int count = 0;
+  struct buf *b = bkt->head.next;
+  while (b != &bkt->head)
+  {
+    if (b->refcnt != 0 || b->blockno != 0 || b->dev != 0 || b->valid != 0) {
+      panic("wrong refcnt");
+    }
+
+    count++;
+    b = b->next;
+  }
+
+  return count;
+}
+
+
+static struct buf* bget(uint dev, uint blockno)
+{
+  acquire(&bcache.lock);
+
+/*   if (blockno == 889) {
+    log889.bget_889++;
+  } */
+
+  int index = blockno % TABLE_SIZE;
+  struct bucket *bkt = &bcache.buf_table[index];
+
+  struct buf *b;
+
+  // acquire(&bkt->lock);
+  // Is the block cached?
+  for (b = bkt->head.next; b != &(bkt->head); b = b->next) {  // 4.
+    if (b->dev == dev && b->blockno == blockno) {
+      if (b->valid != 1) {
+        panic("wrong valid");
+      }
+
+      if (b->refcnt < 0) {
+        panic("wrong refcnt");
+      }
+
+      b->refcnt++;
+      // release(&bkt->lock);
+      release(&bcache.lock);
+      acquiresleep(&b->lock);
+      return b;
+    }
+  }
+
+  // release(&bkt->lock);
+  // not cached
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    bkt = &bcache.buf_table[i];
+    // acquire(&bkt->lock);
+    for (b = bkt->head.next; b != &(bkt->head); b = b->next) {   // 3. 
+      if (b->refcnt == 0) {
+        b->blockno = blockno;
+        b->dev = dev;
+        b->valid = 0;
+        b->refcnt++;
+        // release(&bkt->lock);
+        release(&bcache.lock);
+        acquiresleep(&b->lock);
+        return b;
+      }
+    }
+
+    // release(&bkt->lock);  // 1. important!
+
+  }
+
+  panic("bget: no buffers");
+}
+
+
+void brelse(struct buf *b)
+{
+  acquire(&bcache.lock);
+
+  if(!holdingsleep(&b->lock))
+    panic("brelse");
+
+  releasesleep(&b->lock);
+
+/*   if (b->blockno == 889) {
+    log889.brealese_889--;
+  } */
+
+/*   int id = b->blockno % TABLE_SIZE;
+  struct bucket *bkt = &bcache.buf_table[id];
+
+  acquire(&bkt->lock); */
+  if (b->refcnt <= 0) {
+    printf("b->refcnt = %d\n", b->refcnt);
+    panic("wrong brealese");
+    // struct proc* p = myproc();
+  }
+  b->refcnt--;
+  // release(&bkt->lock);
+
+  release(&bcache.lock);
+}
+
+
+struct buf* bread(uint dev, uint blockno)
+{
+  struct buf *b;
+
+  b = bget(dev, blockno);
+  if(!b->valid) {
+    virtio_disk_rw(b, 0);
+    b->valid = 1;
+  }
+
+  return b;
+}
+
+
+void bwrite(struct buf *b)
+{
+  if(!holdingsleep(&b->lock))
+    panic("bwrite");
+  virtio_disk_rw(b, 1);
+}
+
+
+void bpin(struct buf *b) {
+  acquire(&bcache.lock);
+
+/*   int id = b->blockno % TABLE_SIZE;
+  struct bucket *bkt = &bcache.buf_table[id];
+
+  acquire(&bkt->lock); */
+  if (b->refcnt < 0) {
+    panic("wrong bpin");
+  }
+
+/*   if (b->blockno == 889) {
+    log889.bpin_889++;
+  } */
+  b->refcnt++;
+  
+  // release(&bkt->lock);
+
+  release(&bcache.lock);
+}
+
+
+void bunpin(struct buf *b) {
+  acquire(&bcache.lock);
+
+/*   int id = b->blockno % TABLE_SIZE;
+  struct bucket *bkt = &bcache.buf_table[id];
+
+  acquire(&bkt->lock); */
+  if (b->refcnt <= 0) {
+    panic("wrong bunpin");
+  }
+
+/*   if (b->blockno == 889) {
+    log889.brealese_889--;
+  } */
+  b->refcnt--;
+  // release(&bkt->lock);
+
+  release(&bcache.lock);
+}
+
+#endif
