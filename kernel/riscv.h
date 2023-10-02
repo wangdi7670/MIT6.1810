c3914a16a6566dda1829287d9a0d3751d7529cd2
WangDi <wangdi7670@163.com>
2023-10-02 21:59:56 +0800

    添加PTE_COW和PTE_OLD_W

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -343,6 +343,8 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // user can access
+#define PTE_COW    (1L << 8)  // is COW-mapping page?
+#define PTE_OLD_W  (1L << 9)  // old PTE_W is 1 or 0 if it is a COW-mapping
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
